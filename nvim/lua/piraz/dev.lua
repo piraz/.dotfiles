a3208a34b877dab52020f13a1d491bc5581d9540
Flavio Garcia <piraz@candango.org>
2023-02-18 02:58:14 -0500

    Add get_path function to the project

--- nvim/lua/piraz/dev.lua
+++ nvim/lua/piraz/dev.lua
@@ -7,4 +7,10 @@ M.log = require("plenary.log").new({
     use_console = false,
 })
 
+-- from: https://stackoverflow.com/a/9102300/2887989
+M.get_path = function(file_path, sep)
+    sep=sep or "/"
+    return file_path:match("(.*"..sep..")")
+end
+
 return M
