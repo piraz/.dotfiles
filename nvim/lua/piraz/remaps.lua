df4cda68ba416bd211d8c4dc5f8adccaae30307f
Flavio Garcia <piraz@candango.org>
2023-02-18 02:56:44 -0500

    Fix copy remap

--- nvim/lua/piraz/remaps.lua
+++ nvim/lua/piraz/remaps.lua
@@ -1,18 +1,19 @@
-local keymap = require("piraz.keymap");
+local keymap = require("piraz.keymap")
 
-local nnoremap = keymap.nnoremap;
-local vnoremap = keymap.vnoremap;
+local nnoremap = keymap.nnoremap
+local vnoremap = keymap.vnoremap
 
-vim.g.mapleader = " ";
--- nnoremap("<leader>bls", "<cmd>ls<CR><cmd>b ");
-nnoremap("<leader>ee", "<cmd>Ex<CR>");
-nnoremap("<leader>ls", "<cmd>ls<CR>");
-nnoremap("<leader>so", "<cmd>so<CR>");
-nnoremap("<leader>pks", "<cmd>PackerSync<CR>");
+
+vim.g.mapleader = " "
+-- nnoremap("<leader>bls", "<cmd>ls<CR><cmd>b ")
+nnoremap("<leader>ee", "<cmd>Ex<CR>")
+nnoremap("<leader>ls", "<cmd>ls<CR>")
+nnoremap("<leader>so", "<cmd>so<CR>")
+nnoremap("<leader>pks", "<cmd>PackerSync<CR>")
 
 
 -- see: https://stackoverflow.com/a/73354675/2887989
-vnoremap("<leader>cp", '<cmd>"+y<esc><CR>');
+vnoremap("<leader>cp", '"+y<Esc>')
 
 -- From: https://superuser.com/a/310424
 -- TODO: See if this good: nnoremap()
