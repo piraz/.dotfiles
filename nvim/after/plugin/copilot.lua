04ded057afa2470bf37d0f2c12a8f50247974c25
Flavio Garcia <piraz@candango.org>
2023-02-18 03:03:01 -0500

    Remove copilot accept remap
    It was making any space while inserting to wait for the copilot
    remap. Making it anoying to type.

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_set_keymap("i", "<leader>ca", "copilot#Accept('<CR>')", {expr=true, silent=true})
+-- vim.api.nvim_set_keymap("i", "<leader>ca", "copilot#Accept('<CR>')", {expr=true, silent=true})
 vim.keymap.set("n", "<leader>cod", function() vim.cmd("Copilot disable") end)
 vim.keymap.set("n", "<leader>coe", function() vim.cmd("Copilot enable") end)
 vim.keymap.set("n", "<leader>cos", function() vim.cmd("Copilot status") end)
