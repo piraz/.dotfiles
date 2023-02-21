1ea4c7819dad4e035a21a9f28c691837ca22d437
Flavio Garcia <piraz@candango.org>
2023-02-21 12:58:38 -0500

    Add fold remap to gitgutter

--- nvim/after/plugin/gitgutter.lua
+++ nvim/after/plugin/gitgutter.lua
@@ -1,13 +1,14 @@
 local Dev = require("piraz.dev")
 local log = Dev.log
 
-local git_gutter_call = pcall(vim.cmd, "GitGutterDisable")
-if git_gutter_call then
+local loaded, _ = pcall(vim.cmd, "GitGutterDisable")
+if loaded then
     vim.cmd("GitGutterEnable")
-    vim.keymap.set("n", "<leader>ghp", "<Plug>(GitGutterPreviewHunk)")
-    vim.keymap.set("x", "<leader>ghs", "<Plug>(GitGutterStageHunk)")
-    vim.keymap.set("n", "<leader>ghs", "<Plug>(GitGutterStageHunk)")
-    vim.keymap.set("n", "<leader>ghu", "<Plug>(GitGutterUndoHunk)")
+    vim.keymap.set("n", "<leader>ggp", "<Plug>(GitGutterPreviewHunk)")
+    vim.keymap.set("n", "<leader>ggs", "<Plug>(GitGutterStageHunk)")
+    vim.keymap.set("x", "<leader>ggs", "<Plug>(GitGutterStageHunk)")
+    vim.keymap.set("n", "<leader>ggu", "<Plug>(GitGutterUndoHunk)")
+    vim.keymap.set("n", "<leader>ggf", "<Plug>(GitGutterFold)")
 else
     log.debug("vim.gitgutter not found")
 end
