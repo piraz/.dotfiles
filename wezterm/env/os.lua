9c727c269f2aeee122fa6f8049acc85e552863d2
Flavio Garcia <piraz@candango.org>
2023-02-21 18:14:29 -0500

    Fix os arch name detection

--- wezterm/env/os.lua
+++ wezterm/env/os.lua
@@ -1,4 +1,5 @@
 -- Inspired by:
+
 -- https://github.com/KevinSilvester/wezterm-config/blob/master/utils/
 -- get_os_name.lua
 
@@ -9,47 +10,47 @@ function M._clean_names(raw_os_name, raw_os_arch)
     raw_os_name = (raw_os_name):lower()
     raw_os_arch = (raw_os_arch):lower()
 
-   -- print( ("Debug: %q %q"):format( raw_os_name, raw_arch_name) )
+    local os_name, arch_name = "unknown", "unknown"
 
-   local os_patterns = {
-      ["windows"] = "Windows",
-      ["linux"] = "Linux",
-      ["osx"] = "Darwin",
-      ["mac"] = "Darwin",
-      ["darwin"] = "Darwin",
-      ["^mingw"] = "Windows",
-      ["^cygwin"] = "Windows",
-      ["bsd$"] = "BSD",
-      ["sunos"] = "Solaris",
-   }
+    -- print( ("Debug: %q %q"):format( raw_os_name, raw_arch_name) )
 
-   local arch_patterns = {
-      ["^x86$"] = "x86",
-      ["i[%d]86"] = "x86",
-      ["amd64"] = "x86_64",
-      ["x86_64"] = "x86_64",
-      ["x64"] = "x86_64",
-      ["power macintosh"] = "powerpc",
-      ["^arm"] = "arm",
-      ["^mips"] = "mips",
-      ["i86pc"] = "x86",
-   }
+    local os_patterns = {
+        ["windows"] = "Windows",
+        ["linux"] = "Linux",
+        ["osx"] = "Darwin",
+        ["mac"] = "Darwin",
+        ["darwin"] = "Darwin",
+        ["^mingw"] = "Windows",
+        ["^cygwin"] = "Windows",
+        ["bsd$"] = "BSD",
+        ["sunos"] = "Solaris",
+    }
 
-   local os_name, arch_name = "unknown", "unknown"
+    local arch_patterns = {
+        ["^x86$"] = "x86",
+        ["i[%d]86"] = "x86",
+        ["amd64"] = "x86_64",
+        ["x86_64"] = "x86_64",
+        ["x64"] = "x86_64",
+        ["power macintosh"] = "powerpc",
+        ["^arm"] = "arm",
+        ["^mips"] = "mips",
+        ["i86pc"] = "x86",
+    }
 
-   for pattern, name in pairs(os_patterns) do
-      if raw_os_name:match(pattern) then
-         os_name = name
-         break
-      end
-   end
-   for pattern, name in pairs(arch_patterns) do
-      if raw_os_name:match(pattern) then
-         arch_name = name
-         break
-      end
-   end
-   return os_name, arch_name
+    for pattern, name in pairs(os_patterns) do
+        if raw_os_name:match(pattern) then
+            os_name = name
+            break
+        end
+    end
+    for pattern, name in pairs(arch_patterns) do
+        if raw_os_arch:match(pattern) then
+            arch_name = name
+            break
+        end
+    end
+    return os_name, arch_name
 
 end
 
