2105e25a5e2524633b970aeb18e7b9ddd9d612b8
Flavio Garcia <piraz@candango.org>
2023-02-21 18:09:25 -0500

    Change mac font size and other configurations
    Also use the config color to set the color scheme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,15 +1,16 @@
 local wezterm = require("wezterm")
+local color = require("config.color")
 local env_os = require("env.os")
 
 local font_size = 10
 if env_os.is_mac() then
-    font_size = 12
+    font_size = 10
 end
 
 return {
-    font = wezterm.font "JetBrains Mono",
+    font = wezterm.font "JetBrains Mono NL",
     font_size = font_size,
-    color_scheme = "SeaShells",
-    window_background_opacity = 0.88,
+    color_scheme = color.schemes.solar_flare_base16,
+    window_background_opacity = 0.94,
 }
 
