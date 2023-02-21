5153c5cd498c216260ebb93ddb64c5c267739e81
Flavio Garcia <piraz@candango.org>
2023-02-21 12:59:49 -0500

    Add font configuration based on os type

--- nvim/after/plugin/nvim-project.lua
+++ nvim/after/plugin/nvim-project.lua
@@ -1,9 +1,9 @@
 local Dev = require("piraz.dev")
 local log = Dev.log
 
-local nvim_project = pcall(require, "project_nvim")
-if nvim_project then
-    require("project_nvim").setup {
+local loaded, project = pcall(require, "project_nvim")
+if loaded then
+    project.setup {
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,14 @@
 local wezterm = require("wezterm")
+local env_os = require("env.os")
+
+local font_size = 10
+if env_os.is_mac() then
+    font_size = 16
+end
 
 return {
     font = wezterm.font "JetBrains Mono",
+    font_size = font_size,
     color_scheme = "SeaShells",
     window_background_opacity = 0.88,
 }
